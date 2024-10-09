88634c501be4c31650c19fcf89ef2930d3f55326
javixyloque <perosa1000@gmail.com>
2024-10-09 16:05:59 +0200

    Commit trabajo casa

---
+++ U202E03_JavierAlvarezCenteno.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const verificarNumeroTarjeta = numT => {
+    let sum = 0;
+    let cadena = numT.toString();
+    let temp;
+
+    let arrNums = cadena.split('');
+    console.log(arrNums)
+    
+    
+    // DE VERDAD QUE NO SE ME OCURRIA OTRA FORMA DE HACERLO,
+    // CON FOREACH NO ME DEJABA HACERLO
+    let arrOrd = arrNums.map((element, index) => {
+        if (index%2!==0) {
+            temp = parseInt(element*2);
+            if (temp>=10) {
+                temp-=9;
+            }
+            sum+=temp;
+            return temp;
+        } else {
+            sum+=parseInt(element);
+            return parseInt(element);
+        }
+        
+        
+    });
+
+    console.log(arrOrd);
+    
+    console.log(`La suma total de los numeros de la cuenta es ${sum}`);
+    
+
+    if (sum%10===0) {
+        console.log("El numero de cuenta es valido");
+    } else {
+        console.log("El numero de cuenta no es valido");
+    }
+}
+
+
+let numT = 4532015112830366;
+verificarNumeroTarjeta(numT);
